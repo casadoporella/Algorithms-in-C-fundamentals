3cae1b289ed11495ec3c93c70b6b534eb54a312e
krmboya <krmboya@gmail.com>
2012-10-23 07:32:04 +0300

    added linked list implementation

---
+++ linkedlist.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node { int key; struct node *next; };
+struct node *head, *z, *t;
+
+void listinitialize() {
+  head = (struct node *) malloc(sizeof(*head));
+  z = (struct node *) malloc(sizeof(*z));
+  head->next = z; z->next = z;
+}
+
+void deletenext(struct node *x)
+{
+  t = x->next;
+  x->next = x->next->next;
+  free(t);
+}
+
+struct node* insertafter(int v, struct node* t)
+{
+  struct node* x = (struct node*) malloc(sizeof(struct node));
+  x->key = v; 
+  x->next = t->next;
+  t->next = x;
+  return x;
+}
+
+void printkeys()
+{
+  t = head->next;
+  while(t->next != t) {
+    printf("%d ", t->key);
+    t = t->next;
+  }
+}
+
+void movenexttofront(struct node* x)
+{
+  t = x->next;
+  x->next = x->next->next;
+  t->next = head->next;
+  head->next = t;
+}
+
+void exchange(struct node *t, struct node *u)
+{
+  struct node *x, *y, *z;
+  x = t->next;
+  y = t->next->next;
+  z = u->next->next;
+  t->next = u->next;
+  t->next->next = y;
+  u->next = x;
+  u->next->next = z;
+}
+
+int main(int argc, char* argv[])
+{
+  struct node *ptr1, *ptr2;
+  listinitialize();
+  ptr1 = insertafter(1, head);
+  ptr2 = insertafter(2, ptr1);
+  ptr1 = insertafter(3, ptr2);
+  printkeys(); printf("\n");
+  exchange(head, ptr2);
+  printkeys(); printf("\n");
+  movenexttofront(ptr2);
+  printkeys(); printf("\n");
+  deletenext(head);
+  printkeys();
+  return 0;
+}
